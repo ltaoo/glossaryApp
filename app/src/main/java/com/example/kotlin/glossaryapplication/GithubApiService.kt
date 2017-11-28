d86193d2d3ea1dabaf72a8139e0cabebc8f1cb79
ltaoo <litaowork@aliyun.com>
2017-11-28 22:46:48 +0800

    【增加】网络请求 Log 功能

--- app/src/main/java/com/example/kotlin/glossaryapplication/GithubApiService.kt
+++ app/src/main/java/com/example/kotlin/glossaryapplication/GithubApiService.kt
@@ -1,6 +1,8 @@
 package com.example.kotlin.glossaryapplication
 
 import io.reactivex.Observable
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -21,10 +23,20 @@ interface GithubApiService {
      */
     companion object Factory {
         fun create(): GithubApiService {
+            val builder = OkHttpClient.Builder()
+            if (BuildConfig.DEBUG) {
+                // Log信息拦截器
+                val loggingInterceptor = HttpLoggingInterceptor()
+                loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+                //设置 Debug Log 模式
+                builder.addInterceptor(loggingInterceptor)
+            }
+            val okHttpClient = builder.build()
             val retrofit = Retrofit.Builder()
                     .addCallAdapterFactory(retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory.create())
                     .addConverterFactory(retrofit2.converter.gson.GsonConverterFactory.create())
                     .baseUrl("https://api.github.com/")
+                    .client(okHttpClient)
                     .build()
 
             return retrofit.create(GithubApiService::class.java);
